e5c11df536d46493a6b2736922861b8f58448b6b
netop://ウエハ <paul@webb.page>
2025-01-01 19:18:24 -0800

    adds latest blog content

--- src/lib/component/Notes.svelte
+++ src/lib/component/Notes.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   //// var
   const notes = [
+    "010-internet-birthday.txt",
     "009-write-human-code.txt",
     "008-use-your-own-domain-bluesky.txt",
     "007-goals-and-systems-quote.txt",
