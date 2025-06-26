05de4842bb6cdd63ac622c0bc074b5503a2a3c52
netop://ウィビ <paul@webb.page>
2025-06-26 00:52:18 -0700

    new note

--- src/lib/component/Notes.svelte
+++ src/lib/component/Notes.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   //// var
   const notes = [
+    "012-via-macos-keyboard.txt",
     "011-pixel-perfect-ffmpeg-gifs.txt",
     "010-internet-birthday.txt",
     "009-write-human-code.txt",
